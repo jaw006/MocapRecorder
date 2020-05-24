977f4ce3edac1163d957feaf4f7f6c0594076916
jaw006 <jaw006@ucsd.edu>
2020-05-24 01:45:51 -0700

    delete fastik from scene, configure for vive trackers

---
+++ Assets/CopyRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyRotation : MonoBehaviour
+{
+    public Transform transformToCopy;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.rotation = transformToCopy.rotation;
+        
+    }
+}
---
+++ Assets/Scripts/IKScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+namespace MocapRecorder
+{
+    class IKScript :  MonoBehaviour
+    {
+        public KeyCode ResetOrientationKey = KeyCode.R;
+        public GameObject[] Trackers;
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(ResetOrientationKey))
+            {
+                foreach(GameObject tracker in Trackers)
+                {
+                    // Reset transform for each tracker
+                    var parentRot = tracker.transform.parent.rotation;
+                    parentRot = Quaternion.Inverse(parentRot);
+                    tracker.transform.rotation = parentRot;
+                }
+                Debug.Log("Resetting orientation");
+
+            }
+        }
+
+    }
+}
